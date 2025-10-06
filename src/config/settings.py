22bedc4e68cb983b80265b0f1c046b66573a9c75
Miguel00Roza <miguelroza13@gmail.com>
2025-10-06 18:44:29 -0300

    add user registration API with tests and necessary dependencies

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,10 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Neurodivergente',
+    'rest_framework',
+    'corsheaders',
+    'core.Neurodivergente',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # Porta padrão do React
+    "http://127.0.0.1:3000",
+]
+ALLOWED_HOSTS = ['*']
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- src/core/Neurodivergente/apps.py
+++ src/core/Neurodivergente/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NeurodivergenteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Neurodivergente'
+    name = 'core.Neurodivergente'
